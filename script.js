d9def9a3c6b4b1a0bd5e2766902dea0913a59d04
Brandon Gutierrez <goml.brady@gmail.com>
2020-03-11 16:00:27 -0700

    Debating if i want to do this or not

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-let = document.getElementsByClassName(origin);
+let origin = document.getElementsByClassName("origin");
+
+for (let i = 0; i < origin.length; ++i) {
+    origin[i].addEventListener("click", originClick);
+}
+
+function originClick() {
+    let displayText = document.getElementById("display");
+    console.log(this.innerText + " button was clicked");
+    if (displayText === '0') {
+        
+    }
+}
+
+
